110a981e2be6e60d8ba2189b94e4f2e3ef6b03c2
Javier HM <javierhm2004@gmail.com>
2025-07-18 14:33:37 +0200

    Created data and constants files

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const placeholderMsg = 'Write markdown here...'
---
+++ src/data.ts
@@ -0,0 +1,5 @@
+export const github = {
+  name: 'GitHub',
+  logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg',
+  url: 'https://github.com/Croking09',
+}
