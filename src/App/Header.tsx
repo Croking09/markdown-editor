2ee797cc7d7af7ac5b358ec405f15e4f787e0061
Javier HM <javierhm2004@gmail.com>
2025-07-20 12:17:41 +0200

    Implemented opening local .md file

--- src/App/Header.tsx
+++ src/App/Header.tsx
@@ -6,7 +6,7 @@ import lightModeIcon from '../assets/light-mode.svg'
 
 function Header() {
   const { isDark, toggleDarkMode } = useDarkMode()
-  const { text } = useText()
+  const { text, setText } = useText()
 
   const handleDownload = () => {
     const blob = new Blob([text], { type: 'text/markdown;charset=utf-8' })
@@ -20,17 +20,48 @@ function Header() {
     URL.revokeObjectURL(url)
   }
 
+  const handleReadFile = () => {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = 'text/markdown'
+
+    input.onchange = (e: Event) => {
+      const target = e.target as HTMLInputElement
+      const file = target.files?.[0]
+      if (!file) return
+
+      const reader = new FileReader()
+      reader.onload = (e: ProgressEvent<FileReader>) => {
+        const text = e.target?.result
+        if (typeof text === 'string') {
+          setText(text)
+        }
+      }
+      reader.readAsText(file)
+    }
+
+    input.click()
+  }
+
   return (
     <div>
       <div className="p-2 flex items-center justify-between">
         <div className="flex">
           <p className="mr-4">Markdown Editor</p>
-          <button
-            className="text-gray-500 cursor-pointer"
-            onClick={handleDownload}
-          >
-            Save file
-          </button>
+          <div className="flex gap-4">
+            <button
+              className="text-gray-500 cursor-pointer"
+              onClick={handleReadFile}
+            >
+              Open file
+            </button>
+            <button
+              className="text-gray-500 cursor-pointer"
+              onClick={handleDownload}
+            >
+              Save file
+            </button>
+          </div>
         </div>
 
         <img
