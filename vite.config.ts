23a491e8366c0418618921d55556b2c0c4ca4dbf
Javier HM <javierhm2004@gmail.com>
2025-08-02 01:59:03 +0200

    Configured VitePWA and support for offline use

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,69 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
   base: '/markdown-editor/',
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Markdown Editor',
+        short_name: 'MD Editor',
+        description: 'A simple markdown editor',
+        theme_color: '#010e24',
+        icons: [
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "icons/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable"
+          },
+          {
+            src: "icons/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }
+        ],
+        screenshots: [
+          {
+            src: "screenshots/editor-wide.png",
+            sizes: "1919x908",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "screenshots/editor-mobile.png",
+            sizes: "376x815",
+            type: "image/png",
+            form_factor: "narrow",
+          }
+        ],
+        display: "standalone",
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      devOptions: {
+        enabled: true,
+      }
+    })
   ],
 })
