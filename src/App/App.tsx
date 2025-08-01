3b4222e242c9f6e157bd150d138c28a86a889a08
Javier HM <javierhm2004@gmail.com>
2025-08-02 01:58:38 +0200

    Fixed real time resize

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import Header from './Header.tsx'
 import Footer from './Footer.tsx'
@@ -6,7 +6,16 @@ import Editor from '../Editor/Editor.tsx'
 
 function App() {
   const [isPreviewMode, setIsPreviewMode] = useState(false)
-  const isMobileView = window.innerWidth < 640 // tailwind's sm breakpoint
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth < 640) // tailwind's sm breakpoint
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth < 640)
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   return (
     <div className="min-h-screen flex flex-col font-inter bg-light-bg text-dark-text dark:bg-dark-bg dark:text-light-text transition-colors duration-300">
